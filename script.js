9b2bfe77d4e8c6383ff46f7f9fa708a6420173f3
Tapan Rathore <tapanrathore07@gmail.com>
2026-05-03 22:14:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const activeCircles = document.querySelectorAll(".circle.active");
+
+  const progressPercent =
+    ((activeCircles.length - 1) / (circles.length - 1)) * 100;
+
+  progress.style.width = progressPercent + "%";
+
+  // Button states
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
+}
